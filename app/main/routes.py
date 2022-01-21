5b318ea4b1f7b894093cb0afce02697b2ed0aa11
snunzi <schylernunziata@gmail.com>
2022-01-21 12:35:32 -0500

    Format editable cells

--- app/main/routes.py
+++ app/main/routes.py
@@ -73,6 +73,7 @@ def rundata():
 	search = request.args.get('search[value]')
 	if search:
 		query = query.filter(db.or_(
+			Run.description.like(f'%{search}%'),
 			Run.run_id.like(f'%{search}%')
 	))
 	total_filtered = query.count()
@@ -131,38 +132,18 @@ def updatesample(username):
 		db.session.commit()
 		return json.dumps({'status':'OK'})
 
-@bp.route('/user/<username>/CreateSample', methods=['GET', 'POST'])
+@bp.route('/user/<username>/Analyze', methods = ['GET', 'POST'])
 @login_required
-def sample(username):
-	form = CreateSample()
-	if form.validate_on_submit():
-		f = form.sample_read1.data
-		filename = secure_filename(f.filename)
-
-		sample_id = form.sample_id.data + '.fasta'
-		#pathlib.Path(current_app.config['UPLOAD_FOLDER'], sample_id).mkdir(exist_ok=True)
-		f.save(os.path.join(current_app.config['UPLOAD_FOLDER'], sample_id))
-
-		sample = Sample(sample_id=form.sample_id.data, insert_size=form.insert_size.data,  seq_kit=form.seq_kit.data, seq_platform=form.seq_platform.data, seq_location=form.seq_location.data, PE_SE=form.PE_SE.data, author=current_user)
-
-		db.session.add(sample)
-		db.session.commit()
-		flash('Your sample is now uploaded!')
-		return redirect(url_for('main.index'))
-	return render_template("sample.html", user=user, form=form)
-
-@bp.route('/user/<username>/Assemble_Sample', methods = ['GET', 'POST'])
-@login_required
-def assemble_sample(username):
+def analyze(username):
 	if request.method == 'POST':
-		sample_list = request.form.getlist('chkbox')
+		run_list = request.form.getlist('chkbox')
 		with open(os.path.join(current_app.config['CONFIG_FOLDER'], "samples.tsv"), 'w') as filehandle:
 			filehandle.write("sample\n")
 			for listitem in sample_list:
 				filehandle.write('%s\n' % listitem)
 		print(request.form.getlist('chkbox'))
 		return redirect(url_for('main.config', username=current_user.username))
-	return render_template("assemble_sample.html", samples=Sample.query.order_by(Sample.timestamp.desc()).all())
+	return render_template("analyze.html", samples=Sample.query.order_by(Sample.timestamp.desc()).all())
 
 @bp.route('/user/<username>/Config', methods = ['GET', 'POST'])
 @login_required
